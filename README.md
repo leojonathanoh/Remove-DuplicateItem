8cd65525addc4a9c05f97c135209a0f7d34f8d7b
Leo <leonardjonathanoh@live.com>
2016-11-02 15:50:34 +0800

    update readme, optimized script
    update readme
    
    update readme
    
    update readme
    
    update readme
    
    update readme
    
    update readme
    
    optimized script, update readme
    
    optimized script
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    update script and readme

--- README.md
+++ README.md
@@ -1,16 +1,51 @@
-# rmdups - Powershell script to list/move/remove all duplicate files on Windows systems. 
-By default, it logs duplicates in the script directory to file called output.txt for review.
+# rmdups
+## Description
+A simple Powershell script that can be used to list / move / remove duplicate files on Windows systems.
+- It performs a search of a folder and all subfolders, finding duplicate files within each folder, and depending on the mode, can either list, move, or delete (to recycle bin) those duplicate files.
+- It is not meant to be used on a system-wide scale to remove duplicate files distributed in various folders. (It cannot!)
 
-Requirements:
+## Features:
+- Searches through a folder and all decendent folders (i.e. subfolders) for duplicates that exist WITHIN a folder. 
+	- The process is as follows: It begins in one folder, searches for duplicates within this folder, and then does whatever it has to with those duplicate files based on the configuration. The process then repeats on another folder.
+	- e.g. D:\folder_with_dups\1.txt and D:\folder_with_dups\2.txt are identical. The script marks this as identical.
+
+- Choose a mode for one of the following:
+	- list duplicates 
+	- move duplicates to a newly created folder within the folder where duplicates exist
+	- delete duplicates to the recycle bin
+- Logs the entire search session (output.txt) to the script's directory for you to review any duplicates. 
+
+## Features absent:
+- Search is NOT system-wide, but folder-specific : a file in one folder that is identical to an identical file in another folder are not considered duplicates.
+	- e.g. D:\folder_with_dups\1.txt and D:\other_folder_with_dups\2.txt are identical. The script does not consider 2.txt as a duplicate of 1.txt.
+
+## Requirements:
 - Powershell v4
 - Windows environment
+- User with read/write/modify permissions on script and searched directories.
 
-Installation:
--Open the rmdups.ps1 in your favourite text editor and configure the script settings at the top of the script (instructions are included).
--Right click on the script in explorer and select 'Run with Powershell'.
+## Installation/usage:
+- Open the rmdups.ps1 in your favourite text editor and configure the script settings at the top of the script (instructions are included).
+- Right click on the script in explorer and select 'Run with Powershell'. (should be present on Windows 7 and up)
 
-NOTE:
--Script directory (where you save the script) needs write permission by default. This enables the duplicates log to be created by default for your review.
--Directories searched may need read,write,execute,modify permissions (if using mode 1 or 2).
+## NOTE:
+- By default, script directory (where you run the script) needs write permission for session logging (output.txt). If you prefer not to, turn off session logging in script configuration.
+- If using mode 1 or 2, ensure directories searched have read,write,execute,modify permissions.
+
+## FAQ
+What is a duplicate file?:
+- Same file contents (file hash)
+- Same file size
+- Same containing folder
+- Might have completely different file name
+
+What is the original file?:
+- A duplicate file but with the shortest name among all duplicates found.
+
+## Background:
+- Most people create duplicate files within a specific folder either accidentally, or unknowingly, or uninformedly
+- Accidentally: For instance, you create a file, and accidentally press Ctrl-C and Ctrl-V in the wrong place - a possible scenario, in explorer while highlighting on a file or a bunch of files, until you notice a bunch of copied files with appended '-Copy' or '- Copy 1' or '1 - Copy - Copy (1)'.
+- Unknowingly: You are working on a .doc, saving it once. Then a minute later you forgot you saved it, and save it again in another name. Now you have two identical files in a certain folder.
+- Uninformedly: You restore files from Onedrive Web App (e.g. onedrive.com). Then later the same file from Recycle bin. You now have a duplicate in the same folder probably with suffix 'Copy 1' or some sort.
 
 
--- rmdups.ps1
+++ rmdups.ps1
@@ -19,40 +19,51 @@ $output = 1
 # 0 - turn off debugging.
 # 1 - turn on debugging. 
 $debug = 0
-#############################################################
+############################################################# 
+# Get script directory, set as cd
+$scriptDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+Set-Location $scriptDir
+Write-Host "Script directory: $scriptDir" -ForegroundColor Green
+
+# Check for write permissions in script dir
+Try { [io.file]::OpenWrite('output.txt').close() }
+ Catch { Write-Warning "Script directory has to be writeable for logging the search session to output.txt for your review $outputfile" }
+
+# start transcript
+if($output -eq 1) {
+$ErrorActionPreference="SilentlyContinue"
+Stop-Transcript | out-null
+$ErrorActionPreference = "Continue"
+Start-Transcript -path output.txt -append
+}
 
 # Check parameters for invalid characters.
 if(!$(Test-Path $startingdir -PathType Container)) {
     Write-Host 'Invalid starting directory! Must be a folder. ' -ForegroundColor Yellow
     pause; exit
-}if ($startingdir -match '[\*\"\?\<\>\|]'){
+}elseif ($startingdir -match '[\*\"\?\<\>\|]'){
     Write-Host 'Invalid starting directory! May not contain characters: : * ? < > | ' -ForegroundColor Yellow
     pause; exit
-}
-if ($dupdir -match '[\/\\\:\*\"\?\<\>\|]'){
+}elseif ($dupdir -match '[\/\\\:\*\"\?\<\>\|]'){
     Write-Host 'Invalid duplicates temp directory! May not contain characters: / \ : * ? < > | ' -ForegroundColor Yellow
     pause; exit
-}
-if(($mode -gt 2) -or ($mode -lt 0)) { 
+}elseif(($mode -gt 2) -or ($mode -lt 0)) { 
     Write-Host "Invalid mode! May be between 0 and 2. `n0 - list only.   `n1 - move duped files to $dupdir that will be created.  `n2 - delete files to recycle bin." -ForegroundColor Yellow
     pause; exit
-}
-if(($output -gt 1) -or ($output -lt 0)) { 
+}elseif(($output -gt 1) -or ($output -lt 0)) { 
     Write-Host "Invalid output mode! May be between 0 and 1. `n0 - log the duplicates to file.   `n1 - do not log the duplicates to file" -ForegroundColor Yellow
     pause; exit
-}
-if(($debug -gt 1) -or ($debug -lt 0)) { 
+}elseif(($debug -gt 1) -or ($debug -lt 0)) { 
     Write-Host "Invalid debug mode! May be between 0 and 1. `n0 - turn off debugging.   `n1 - turn on debugging." -ForegroundColor Yellow
     pause; exit
 }
-
-# cd to directory where dups might exist
-cd $startingdir
+# Show configuration options to user
+Write-Host "Checking configuration options...Configuation options are valid: `n`t`$startingdir: $startingdir `n`t`$dupdir: $dupdir `n`t`$mode:$mode `n`t`$ouput: $output `n`t`$debug: $debug `n" -ForegroundColor Green
 
 # Begin prompts to user to confirm
-Write-Host "Are you sure you want to run the script? Press x to exit the script now"
+Write-Host "Are you sure you want to run the script? Press x to exit the script now" -ForegroundColor Yellow
 $continue = Read-Host; If ($continue -eq "x" -or $continue -eq "X") {exit}
-Write-Host "Are you absolutely sure you want to run the script? Press x to exit the script now"
+Write-Host "Are you absolutely sure you want to run the script? Press x to exit the script now" -ForegroundColor Yellow
 $continue = Read-Host; If ($continue -eq "x" -or $continue -eq "X") {exit}
 Write-Host "`n[Looking for duplicates...]`n" $cd
 
@@ -64,9 +75,8 @@ $containers += Get-ChildItem -Directory -Path $startingdir -Recurse -Exclude $du
 # For each directory, do
 $containers | ForEach-Object {
     $j=0 # total dup count within this directory
-    $t=0 # total file count within this directory
     $cd = $_.FullName # current directory's full path
-    $cd_hash_dups = @{};  # declare/reset the current directory's dup store
+    $cd_filesmapping = @{};  # declare/reset the current directory's dup store
 
     # store all files found only within this directory
     $files = Get-ChildItem -Path $cd -File | Sort-Object Name
@@ -74,32 +84,30 @@ $containers | ForEach-Object {
    
     # for each file, do
     :oloop foreach($f in $files) {
-        $t++
-        $hash_dups = @{}; # to store matches against this file. format: dupObj => thisObj
-        $i=0 # against this file, number of matches 
-        $f_md5 = Get-FileHash $f.PSPath -Algorithm MD5 # md5 hash of this file
-
         # skip over files already checked
-		if($cd_hash_dups.ContainsKey($f)) {
+		if($cd_filesmapping.ContainsKey($f)) {
             if($debug){Write-Host "`t>Skipping(oloop)......." $f} 
             continue oloop;
         }
 		if($debug){Write-Host "`n[Looking in] -" $cd}
-		
+
+        # reset stores, and get md5 for this file.
+        $hash_dups = @{}; # to store matches against this file. format: dupObj => thisObj
+        $i=0 # against this file, number of matches 
+        $f_md5 = Get-FileHash $f.PSPath -Algorithm MD5 # md5 hash of this file
+        
         # against this file, search through all files for dups
 		:iloop foreach($_ in $files) {          
 			# skip over dups alrdy stored in hash
-		    if($cd_hash_dups.ContainsKey($_)) {if($debug){Write-Host "`t>Skipping(iloop)......." $_} continue iloop}
+		    if($cd_filesmapping.ContainsKey($_)) {if($debug){Write-Host "`t>Skipping(iloop)......." $_} continue iloop}
 
             $_md5 = Get-FileHash $_.PSPath -Algorithm MD5 # md5 hash of other file
             if($debug){Write-Host " - f.Name:" $f.Name "`t _Name" $_.Name}
             if($debug){Write-Host " - f.BaseName:" $f.BaseName "`t _BaseName" $_.BaseName}
-            if($debug){Write-Host " - f.ext:" $f.Extension "`t _.ext:" $_.Extension}
-            if($debug){Write-Host " - f dot count:"  "`t _ dot count:" ([regex]::Matches($_.Name, "\.").Count)}
             if($debug){Write-Host " - f md5:" $f_md5.Hash "`t _md5:" $_md5.Hash}
 
-            # dup is found when: matches BaseName (i.e. removed path/extension), matches on size, last modified, number of periods (i.e. '.'), and hash.
-			if(($_.Length -eq $f.Length) -and ($_.LastWriteTime.DateTime -eq  $f.LastWriteTime.DateTime) -and ($_.Extension -eq $f.Extension) -and ([regex]::Matches($f.Name, "\.").Count -eq ([regex]::Matches($_.Name, "\.").Count)) -and ($f_md5.Hash -eq $_md5.Hash) ) 
+            # dup is found when: matches on size, and matches on md5 hash.
+			if(($_.Length -eq $f.Length) -and ($f_md5.Hash -eq $_md5.Hash) ) 
 			{
 				$i++;
 				# store this first dup in hash
@@ -116,12 +124,12 @@ $containers | ForEach-Object {
 			 }
 		}
 
-		# if file matched itself only (i.e. 1 match) - no duplicates for this file. add to cd_hash to mark as done
+		# if file matched itself only (i.e. 1 match) - no duplicates for this file. add to cd_hash to mark as done, and continue
 		if($i -eq 1) { 
-			$cd_hash_dups.add($f, $f ); # format: thisObj(key) => thisObj(value)
+			$cd_filesmapping.add($f, $f ); # format: thisObj(key) => thisObj(value)
 			if($debug){Write-Host "`t>No dups, Skipping(oloop)......." $_} 
 		    if($debug){echo "-----#g----"}
-		    if($debug){echo $cd_hash_dups}
+		    if($debug){echo $cd_filesmapping}
 			continue oloop;
 		}
 
@@ -147,21 +155,22 @@ $containers | ForEach-Object {
 		foreach($key in $($hash_dups.keys)){
 			$hash_dups[$key] = $f_shortestName
 			# do not add global
-			$cd_hash_dups.Add($key, $hash_dups[$key]) # format: dupObj(key) => oriObj(value)
+			$cd_filesmapping.Add($key, $hash_dups[$key]) # format: dupObj(key) => oriObj(value)
 		}
 		
         # debug
         if($debug){echo $hash_dups}
 		if($debug){echo "-----#cd----"}
-		if($debug){echo $cd_hash_dups}
+		if($debug){echo $cd_filesmapping}
     }
 
     Write-Host "[Duplicates found in] " $cd
 
     if($mode -eq 0) {
         Write-Host "`tdup file`t`t`t`t`toriginal file`n`t----------`t`t`t`t`t--------------"
-        $cd_hash_dups.GetEnumerator() | % {
+        $cd_filesmapping.GetEnumerator() | % {
             if($_.key.ToString() -ne $_.value.ToString()) { # exclude the original which has key==value
+                $j++
                 Write-Host "`t" $_.key "`t`t`t`t`t"  $_.value 
             }
         }
@@ -173,7 +182,7 @@ $containers | ForEach-Object {
             New-Item -ItemType Directory -Force -Path $duppath
         }
         # move files
-        $cd_hash_dups.GetEnumerator() | % { 
+        $cd_filesmapping.GetEnumerator() | % { 
             if($debug){Write-Host "key vs value: " $_.key $_.value}
             if($_.key.ToString() -ne $_.value.ToString()) { # exclude the original which has key==value
                 $j++
@@ -184,7 +193,7 @@ $containers | ForEach-Object {
         }   
     }elseif($mode -eq 2) {
         # delete files to recycle bin
-        $cd_hash_dups.GetEnumerator() | % { 
+        $cd_filesmapping.GetEnumerator() | % { 
             
             if($_.key.ToString() -ne $_.value.ToString()) { # exclude the original which has key==value
                 $j++
@@ -201,16 +210,13 @@ $containers | ForEach-Object {
     }
 	
     # show summary only if dups exist
+    $t = $files.Count # total file count within this directory
     if ($j) {Write-Host " -summary: non-dup count:$($t-$j) vs dup count:$j vs total: $t"}
     
  }
 
+# stop transcript
 if($output -eq 1) {
-$ErrorActionPreference="SilentlyContinue"
-Stop-Transcript | out-null
-$ErrorActionPreference = "Continue"
-Start-Transcript -path output.txt -append
-# do some stuff
 Stop-Transcript
 }
 
