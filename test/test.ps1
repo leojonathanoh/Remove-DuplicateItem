99a755a01ed8afe3b1a46fbefb46365a33abdce6
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2021-05-31 08:12:42 +0000

    Refactor (tests): Update test entrypoint

--- test/test.ps1
+++ test/test.ps1
@@ -1,22 +1,54 @@
-. "$( Split-Path $PSScriptRoot -Parent )/env.ps1"
+param (
+    [string]$Tag = ''
+)
+$MODULE_NAME = (Get-Item $PSScriptRoot/../).Name
+$MODULE_DIR = "$PSScriptRoot/../src/$MODULE_NAME"
+$MODULE_PATH = "$MODULE_DIR/$MODULE_NAME.psm1"
 
 Set-StrictMode -Version Latest
 $global:PesterDebugPreference_ShowFullErrors = $true
 
 # Install Pester if needed
 $pester = Get-Module Pester -ListAvailable -ErrorAction SilentlyContinue
-if ( ! $pester -or $pester.Version.Major -lt 4 ) {
-    Install-Module Pester -Force -Scope CurrentUser
+$pesterMinVersion = [version]'4.0.0'
+$pesterMaxVersion = [version]'4.10.1'
+if ( ! $pester -or ! ($pester.Version | ? { $_ -ge $pesterMinVersion -and $_ -le $pesterMaxVersion }) ) {
+    if ($PSVersionTable.PSVersion.Major -ge 6) {
+        Install-Module Pester -Force -Scope CurrentUser -MinimumVersion $pesterMinVersion -MaximumVersion $pesterMaxVersion -ErrorAction Stop
+    }else {
+        Install-Module Pester -Force -Scope CurrentUser -MinimumVersion $pesterMinVersion -MaximumVersion $pesterMaxVersion -SkipPublisherCheck -ErrorAction Stop
+    }
 }
 Get-Module Pester -ListAvailable
+Import-Module Pester -MinimumVersion $pesterMinVersion -MaximumVersion $pesterMaxVersion -Force -ErrorAction Stop
 
 # Import our module
-Get-Module "$MODULE_NAME" | Remove-Module
-Import-Module "$SRC_MODULE_DIR/$MODULE_NAME.psd1" -Force
+Get-Module "$MODULE_NAME" | Remove-Module -Force
+Import-Module $MODULE_PATH -Force -ErrorAction Stop
 
-# Run tests
-$res = Invoke-Pester -Script $SRC_MODULE_DIR -PassThru
-if ($res.FailedCount -gt 0) {
-    "$( $res.FailedCount ) tests failed."
-    exit 1
+if ($Tag) {
+    # Run Unit Tests
+    $res = Invoke-Pester -Script $MODULE_DIR -Tag $Tag -PassThru -ErrorAction Stop
+    if ($res.FailedCount -gt 0) {
+        "$( $res.FailedCount ) $Tag tests failed." | Write-Host
+    }
+    if ($res -and $res.FailedCount -gt 0) {
+        throw
+    }
+}else {
+    # Run Unit Tests
+    $res = Invoke-Pester -Script $MODULE_DIR -Tag 'Unit' -PassThru -ErrorAction Stop
+    if ($res.FailedCount -gt 0) {
+        "$( $res.FailedCount ) unit tests failed." | Write-Host
+    }
+
+    # Run Integration Tests
+    $res2 = Invoke-Pester -Script $MODULE_DIR -Tag 'Integration' -PassThru -ErrorAction Stop
+    if ($res2.FailedCount -gt 0) {
+        "$( $res2.FailedCount ) integration tests failed." | Write-Host
+    }
+
+    if (($res -and $res.FailedCount -gt 0) -or ($res2 -and $res2.FailedCount)) {
+        throw
+    }
 }
